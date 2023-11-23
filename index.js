4a7292b8a5da3e708eb9dd8864fbbd31d3960354
Ben Noble <ben.noble@madetech.com>
2023-11-23 15:50:48 +0000

    Basic Lambda function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function helloNpm() {
-    return "hello NPM"
-  }
-  
-  module.exports = helloNpm
+'use strict';
+    
+exports.handler = function(event, context, callback) {
+    if (event.body) {
+        event = JSON.parse(event.body);
+    }
+
+    var sc = 200; // Status code
+    var result = {
+        "text": "Hello world!"
+    };
+
+    const response = {
+        statusCode: sc,
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify( result )
+    };
+
+    callback(null, response);
+};
